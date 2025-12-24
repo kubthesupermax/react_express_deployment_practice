4b3a2a1e7387669fe0b98b7eef96beaa215040f5
kubthesupermax <franklinkuba7@gmail.com>
2025-12-23 21:02:07 -0600

    third commit

--- server.js
+++ server.js
@@ -4,14 +4,26 @@ const app = express();
 
 app.use(express.static("dist"));
 
+const users = [
+  { id: 1, name: "John Doe", age: 32 },
+  { id: 2, name: "Jane Doe", age: 30 },
+  { id: 3, name: "Bob Smith", age: 28 },
+];
+
 // get a list of all users
 app.get("/api/users", (req, res) => {
+  if (req.query.search) {
+    const filteredUsers = users.filter(
+      (user) =>
+        user.name.toLowerCase().includes(req.query.search.toLowerCase()) ||
+        user.age.toString() === req.query.search
+    );
+
+    res.send(filteredUsers);
+    return;
+  }
   setTimeout(() => {
-    res.send([
-      { id: 1, name: "John Doe", age: 32 },
-      { id: 2, name: "Jane Doe", age: 30 },
-      { id: 3, name: "Bob Smith", age: 28 },
-    ]);
+    res.send(users);
   }, 1000);
 });
 
